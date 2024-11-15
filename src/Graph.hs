e5769ccf0c07d2dc472211c49a4a8898c5a3d2d5
Jörg Dölfer <doelfer@gmail.com>
2024-11-15 11:36:27 +0100

    added graph basics

---
+++ src/Graph.hs
@@ -0,0 +1,28 @@
+module Graph where
+
+import qualified Data.AssocMap as M
+import qualified Data.List as L
+
+type DiGraph a = M.AssocMap a [a]
+
+empty :: DiGraph a
+empty = M.empty
+
+addEdge :: (Eq a) => (a, a) -> DiGraph a -> DiGraph a
+addEdge (node, child) = M.alter insertEdge node
+  where
+    insertEdge Nothing = Just [child]
+    insertEdge (Just nodes) =
+      Just (L.nub (child : nodes))
+
+addEdges :: (Eq a) => [(a, a)] -> DiGraph a -> DiGraph a
+addEdges edges graph = foldr addEdge graph edges
+
+buildDiGraph :: (Eq a) => [(a, [a])] -> DiGraph a
+buildDiGraph nodes = go nodes M.empty
+  where
+    go [] graph = graph
+    go ((key, value) : xs) graph = M.insert key value (go xs graph)
+
+children :: (Eq a) => a -> DiGraph a -> [a]
+children = M.findWithDefault []
