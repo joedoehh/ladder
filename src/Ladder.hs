3b7ff0246b4beaeb71fcdd6649b9b1701f6baee1
Jörg Dölfer <doelfer@googlemail.com>
2024-11-17 11:23:20 +0100

    finished datastructures

--- src/Ladder.hs
+++ src/Ladder.hs
@@ -1,5 +1,6 @@
 module Ladder
   ( Dictionary,
+    mkLadderGraph,
     readDictionary,
   )
 where
@@ -17,3 +18,22 @@ readDictionary filepath = do
     lines = L.lines dictionaryContent
     words = L.map (L.filter (`L.elem` ['a' .. 'z'])) lines
   return (L.nub words)
+
+mkLadderGraph :: Dictionary -> G.DiGraph String
+mkLadderGraph dict = G.buildDiGraph nodes
+  where
+    map = PM.createPermutationMap dict
+    nodes =
+      L.map (\w -> (w, computeCandidates map w)) dict
+
+computeCandidates :: PM.PermutationMap -> String -> [String]
+computeCandidates map word =
+  let candidates = modified ++ removed ++ added ++ [word]
+      uniques = L.nub [L.sort w | w <- candidates]
+      perms = L.concatMap (\x -> PM.findWithDefault [] x map) uniques
+   in L.delete word perms
+  where
+    added = [x : word | x <- ['a' .. 'z']]
+    removed = [L.delete x word | x <- word]
+    modified =
+      [x : L.delete y word | x <- ['a' .. 'z'], y <- word, x /= y]
